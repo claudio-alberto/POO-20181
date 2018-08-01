5a70ba5a46055db9d32dba8305ce5c48e573b3cd
claudio_alberto <38073359+claudio-alberto@users.noreply.github.com>
2018-08-01 15:08:40 -0300

    Add files via upload

---
+++ Animal-dog-gato-prova/Animal.java
@@ -0,0 +1,12 @@
+
+public class Animal {
+    private String nome;
+    private int idade;
+    private String som;
+    private String raca;
+    private double peso;
+    
+    public void Som(){
+        System.out.println("Nada a emitir!");
+    }
+}
---
+++ Animal-dog-gato-prova/Prova2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fix Tecnologia
+ */
+public class Prova2 {
+    public static void main(String[] args) {
+       Animal [] animal = new Animal[5];
+       Gato gato1 = new Gato();
+       gato1.Som();
+        
+        // TODO code application logic here
+    }
+    
+}
